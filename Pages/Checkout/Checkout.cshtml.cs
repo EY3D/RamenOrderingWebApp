a69d83ce5267dcaba2d13fce17662f1b86ea0e7f
Esmond Yeo <esmond_yeo@a.com>
2022-10-08 16:56:13 +0800

    modified customise page

--- Pages/Checkout/Checkout.cshtml.cs
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RamenOrderingWebApp.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string RamenName { get; set; }
+        public float RamenPrice { get; set; }
+        public string ImageTitle { get; set; }
         public void OnGet()
         {
+            if(string.IsNullOrEmpty(RamenName))
+            {
+                RamenName = "My ramen";
+            }
+            if(string.IsNullOrEmpty(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
--- Pages/RamenMenu.cshtml.cs
+++ Pages/RamenMenu.cshtml.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RamenOrderingWebApp.Models;
 
 namespace RamenOrderingWebApp.Pages
 {
     public class RamenMenuModel : PageModel
     {
+        public List<RamenModel> fakeRamenDB = new()
+        {
+            new RamenModel()
+            {
+
+            }
+        };
         public void OnGet()
         {
         }
